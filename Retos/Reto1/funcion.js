c74bb5e4dd81d7b40e55d547076087be12f57f47
Bersek3 <143874680+Bersek3@users.noreply.github.com>
2023-09-15 13:01:46 -0300

    Codigo

--- Retos/Reto1/funcion.js
+++ Retos/Reto1/funcion.js
@@ -1,8 +1,16 @@
+
 var numero = parseInt(prompt("Ingrese un numero"));
 
-if (numero % 2 == 0){
-    alert("El numero es par");
+if(!isNaN(numero)){
+    var entrada = parseInt(numero);
 
-}   else{
+    if (numero % 2 === 0){
+        alert("El numero es par");
+
+    } else{
         alert("El numero es impar");
     }
+
+ } else{
+        alert("Ingrese un numero valido");
+}

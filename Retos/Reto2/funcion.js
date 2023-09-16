b9aa8030f8c72ad3773ccba94fcf0f9e428cb8a3
Bersek3 <143874680+Bersek3@users.noreply.github.com>
2023-09-15 23:36:18 -0300

    codigo

--- Retos/Reto2/funcion.js
+++ Retos/Reto2/funcion.js
@@ -1,13 +1,32 @@
-var nota1 = parseInt(prompt("Ingresa la nota 1"));
-var nota2 = parseInt(prompt("Ingresa la nota 2"));
-var nota3 = parseInt(prompt("Ingresa la nota 3"));
+function calcularPromedio() {
+    const nota1 = parseFloat(prompt("Ingrese la nota 1 (1.0 al 7.0):"));
+    const nota2 = parseFloat(prompt("Ingrese la nota 2 (1.0 al 7.0):"));
+    const nota3 = parseFloat(prompt("Ingrese la nota 3 (1.0 al 7.0):"));
+  
+    if (isNaN(nota1) || isNaN(nota2) || isNaN(nota3) ||
+        nota1 < 1.0 || nota1 > 7.0 ||
+        nota2 < 1.0 || nota2 > 7.0 ||
+        nota3 < 1.0 || nota3 > 7.0) {
+      alert("Ingrese notas válidas en el rango de 1.0 a 7.0.");
+      return;
+    }
+  
 
-var promedio = (nota1 * 0.4 + nota2 * 0.3 + nota3 * 0.3);
-
-if (promedio < 3.95){
-    alert("a reprobado la asignatura");
-} else if (promedio >= 3.95 && promedio <= 4.94){
-    alert("a aprobado la asignatira");
-} else {
-    alert("el alumno se exime del examen");
-}
+    const promedio = (nota1 * 0.4 + nota2 * 0.3 + nota3 * 0.3).toFixed(1);
+  
+    if (promedio < 3.95) {
+      alert(`Promedio ponderado: ${promedio}\nEl estudiante ha reprobado`);
+    } else if (promedio >= 3.95 && promedio <= 4.94) {
+      alert(`Promedio ponderado: ${promedio}\nEl estudiante rinde examen.`);
+    } else {
+      alert(`Promedio ponderado: ${promedio}\nEximido de la asignatura.`);
+    }
+  }
+  
+  calcularPromedio();
+  
+  
+  
+  
+  
+  

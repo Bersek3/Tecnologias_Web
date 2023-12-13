6769c33a1720e9a8375b5d4b42bdbc6e82ed8bb2
Bersek3 <143874680+Bersek3@users.noreply.github.com>
2023-12-13 17:59:02 -0300

    Actualizado

--- Guias/Guia1/Guias/ejercicio2.js
+++
@@ -1,6 +0,0 @@
-const oracion = prompt("Ingrese una oración: ");
-const palabras = oracion.split(" ");
-const contador = palabras.length;
-
-document.write(`oración ingresada: ${oracion}<br>`);
-document.write(`número de palabras: ${contador}`);
